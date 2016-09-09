aa90fcd65a2aa64c1f4bf86ee9f9eb5c8efc7ba9
Fang Lu <cc2lufang@gmail.com>
2016-09-09 08:09:56 -0500

    Render in ISR callback
    Improve quality & response time

--- midi2wave.ino
+++ midi2wave.ino
@@ -13,50 +13,50 @@
 short ptr;
 
 void setupMidi() {
-	for (int i=0; i<WAVEBUF_SIZE; ++i)
-		wave[i] = 0;
+	for (int i=0; i<MAX_NOTE; ++i)
+		key_vels[i] = 0;
+	for (char i=0; i<KEYBUF_SIZE; ++i)
+		active_keys[i] = 0;
 	ptr = 0;
 }
 
 void renderWaveBuffer() {
-	//digitalWrite(12, HIGH);
-	char note_count = 0;
 	unsigned char leds = 0;
-	for (int i=0; i<MAX_NOTE; ++i) {
-		if (key_vels[i]) {
-			if (!note_count) {
-				key = i+1;
-			}
+	note_count = 0;
+	// Update active key buffer with new keys
+	// Preserve original index if the key is not released
+	for (char i = 0; i < KEYBUF_SIZE; ++i)
+		if (key_vels[active_keys[i]]) {
+			key_vels[active_keys[i]] |= 32; // the 6th bit is used as a "key-not-released" flag
+			leds |= 1<<(active_keys[i]%6);
 			++note_count;
+		}
+		else {
+			active_keys[i] = 0; // Clear keys that are released
+		}
+
+	for (int i=MAX_NOTE-1; i>=0; --i) // Goes down: make sure the most significant note gets played
+		if ( key_vels[i] && !(key_vels[i]&32) ) {
+			for (char j=0; j<KEYBUF_SIZE; j++)
+				if (!active_keys[j])
+				{
+					active_keys[j] = i;
+					++note_count;
+					break;
+				}
 			leds |= 1<<(i%6);
 		}
-	}
 
-	if (!note_count)
-		for (int i=0; i<WAVEBUF_SIZE; ++i)
-			wave[i] = 0;
+	for (char i = 0; i < KEYBUF_SIZE; ++i)
+		if (active_keys[i])
+			key_vels[active_keys[i]] &= 31; // Clear out the 6th bit "key-not-released" flag
 
-	for (int i=0; i<MAX_NOTE; ++i) {
-		if (key_vels[i]) {
-			if (i==key-1)
-				for (int j = 0; j < WAVEBUF_SIZE; ++j)
-					wave[j] =  pgm_read_byte_near( sine+( j & ( SINE_SAMPLE_SIZE-1 ) ) )*key_vels[i]/16/note_count;
-			else
-				for (int j = 0; j < WAVEBUF_SIZE; ++j)
-					wave[j] +=  pgm_read_byte_near( sine+( ( j * (int)pow( 1.0594630943592952645618252949463, i-key+1 ) ) & ( SINE_SAMPLE_SIZE-1 ) ) )*key_vels[i]/16/note_count;
-		}
-	}
-
-	for (int i = 0; i < 7; ++i)
-	{
-		// Switch LEDs
+	// Set LEDs
+	for (char i = 0; i < 7; ++i)
 		if (leds & (1<<i))
 			sbi(PORTD, i+2);
 		else
 			cbi(PORTD, i+2);
-	}
-
-	//digitalWrite(12, LOW);
 }
 
 void loadNextEvent() {
@@ -66,8 +66,11 @@ void loadNextEvent() {
 		Serial.println("Ended.");
 		setupMidi();
 		event_length = 3000;
+		for (int i = 0; i < 7; ++i)
+			sbi(PORTD, i+2);
 		return;
 	}
+
 	int new_length = NOTE_DELAY(ptr)*TEMPO;
 	key_vels[NOTE_NUMBER(ptr)-1] = NOTE_VEL(ptr);
 
@@ -76,7 +79,6 @@ void loadNextEvent() {
 		loadNextEvent();
 	else {
 		renderWaveBuffer();
-		event_length = new_length/4;
-
+		event_length = new_length;
 	}
 }
